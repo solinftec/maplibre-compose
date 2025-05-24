1747c7e30d5bfc819fada93580d64c6711b7a185
Sargun Vohra <sargunv@users.noreply.github.com>
2025-05-25 01:37:50 +0200

    fix: work around native crash with retrieving ios attribution info during rotation (#357)

--- lib/maplibre-compose/src/iosMain/kotlin/dev/sargunv/maplibrecompose/core/source/Source.kt
+++ lib/maplibre-compose/src/iosMain/kotlin/dev/sargunv/maplibrecompose/core/source/Source.kt
@@ -8,13 +8,13 @@ public actual sealed class Source {
   internal abstract val impl: MLNSource
   internal actual val id: String by lazy { impl.identifier }
 
-  public actual val attributionLinks: List<AttributionLink>
-    get() =
-      (impl as? MLNTileSource)?.attributionInfos?.mapNotNull {
-        it as MLNAttributionInfo
-        if (it.URL == null) return@mapNotNull null
-        AttributionLink(title = it.title.string(), url = it.URL.toString())
-      } ?: emptyList()
+  public actual val attributionLinks: List<AttributionLink> by lazy {
+    (impl as? MLNTileSource)?.attributionInfos?.mapNotNull {
+      it as MLNAttributionInfo
+      if (it.URL == null) return@mapNotNull null
+      AttributionLink(title = it.title.string(), url = it.URL.toString())
+    } ?: emptyList()
+  }
 
   override fun toString(): String = "${this::class.simpleName}(id=\"$id\")"
 }
