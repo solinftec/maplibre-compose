45c782ece79a858187302e2970cb67e0aaa44860
Sargun Vohra <sargunv@users.noreply.github.com>
2025-05-26 19:50:01 -0700

    feat: support drawing Painter as SDF (#373)

---
+++ lib/maplibre-compose/src/androidMain/kotlin/dev/sargunv/maplibrecompose/core/util/ImageBitmap.kt
@@ -0,0 +1,9 @@
+package dev.sargunv.maplibrecompose.core.util
+
+import android.graphics.Bitmap
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+
+internal actual fun IntArray.toImageBitmap(width: Int, height: Int): ImageBitmap {
+  return Bitmap.createBitmap(this, width, height, Bitmap.Config.ARGB_8888).asImageBitmap()
+}
---
+++ lib/maplibre-compose/src/commonMain/kotlin/dev/sargunv/maplibrecompose/core/util/ImageBitmap.kt
@@ -0,0 +1,5 @@
+package dev.sargunv.maplibrecompose.core.util
+
+import androidx.compose.ui.graphics.ImageBitmap
+
+internal expect fun IntArray.toImageBitmap(width: Int, height: Int): ImageBitmap
